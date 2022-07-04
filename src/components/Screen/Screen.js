0b32d5204255cc27a91bc4531576fc7b88859569
Eduard B <edd.bba@gmail.com>
2022-07-04 10:50:06 +0100

    component architecture

---
+++ src/components/Screen/Screen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Screen.scss";
+
+const Screen = () => {
+  return (
+    <div className="img-wrapper">
+      <div className="word-wrapper">
+        <p className="secondary-text">GUESS THE WORD:</p>
+        <div className="word-container">BACK_ROUND</div>
+      </div>
+    </div>
+  );
+};
+
+export default Screen;
